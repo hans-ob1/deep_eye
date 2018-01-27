662d76faf6cfaaa410919474f22d8a0fc2ac2cb3
haixun00 <haixun00@gmail.com>
2018-01-27 15:05:18 +0800

    ipcam

---
+++ ipcamera.py
@@ -0,0 +1,23 @@
+import urllib.request
+import cv2
+import numpy as np
+
+'''
+Note for IP camera:
+Different supplier has different video feed url, look up on zoneminderwiki
+for Dlink ip cameras: https://wiki.zoneminder.com/D-Link#DCS-5222L
+
+'''
+
+DLINK_DCS_5222L_URL = "http://admin:asd123@192.168.0.103:80/video/mjpg.cgi"
+cap = cv2.VideoCapture()
+cap.open(DLINK_DCS_5222L_URL)
+
+while cap.isOpened():
+
+	ret,img = cap.read()
+
+	cv2.imshow('test',img)
+
+	if ord('q') == cv2.waitKey(10):
+		exit(0)
